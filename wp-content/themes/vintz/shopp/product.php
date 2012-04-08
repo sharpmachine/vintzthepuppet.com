7e60fe0cc346b76644b54ac8b3c4ff291399bb2b
Jesse Kade <jessekade@gmail.com>
2012-04-08 11:53:23 -0700

    made Laurens requested changes

--- wp-content/themes/vintz/shopp/product.php
+++ wp-content/themes/vintz/shopp/product.php
@@ -5,7 +5,7 @@
 
 	<h3><?php shopp('product','name'); ?></h3>
 	<p class="headline"><big><?php shopp('product','summary'); ?></big></p>
-
+<div style="clear: both">&nbsp</div>
 	<?php if (shopp('product','onsale')): ?>
 		<h3 class="original price"><?php shopp('product','price'); ?></h3>
 		<h3 class="sale price"><?php shopp('product','saleprice'); ?></h3>
